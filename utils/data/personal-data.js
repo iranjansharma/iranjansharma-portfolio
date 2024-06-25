156179d94b1a92391f7cdcc2474493ea2276ae7f
Ranjan sharma <just.think2019@gmail.com>
2024-06-25 16:54:02 +0530

    Portfolio

---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "RANJAN SHARMA",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description:
+    "My name is RANJAN SHARMA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "iranjansharma12@gmail.com",
+  phone: "+91-8178-540-300",
+  address: "Dwarka, New Delhi, India - 110071 ",
+  github: "https://github.com/iranjansharma",
+  facebook: "https://www.facebook.com//",
+  linkedIn: "https://www.linkedin.com/in//",
+  twitter: "https://twitter.com/",
+  stackOverflow: "https://stackoverflow.com/users/",
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "iranjansharma",
+  resume: "#",
+};
