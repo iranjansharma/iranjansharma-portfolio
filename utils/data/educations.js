6d0f3c33e507b38cd9e3b0cc251154fe2cf1a520
Ranjan sharma <just.think2019@gmail.com>
2024-07-19 22:46:53 +0530

    edu

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,20 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2019 - 2023",
+    institution:
+      "University School of Information, Communication and Technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2017 - 2019",
+    institution: "Govt Boys Senior Secondary School, Najafgarh",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
-]
+    institution: "New Sainik Public School, Qutub Vihar",
+  },
+];
