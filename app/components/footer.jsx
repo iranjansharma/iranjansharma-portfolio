b96d18598973a265f4e475f5a6915b04dc89cafe
Ranjan sharma <just.think2019@gmail.com>
2024-09-09 14:57:47 +0530

    Dockerfile Added

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col items-center justify-between md:flex-row">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/iniranjshrma/" className="text-[#16f2b3]">Ranjan Sharma</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/iranjansharma" className="text-[#16f2b3]">Ranjan Sharma</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -10,11 +10,11 @@ function ProjectCard({ project }) {
         <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
         <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
       </div>
-      <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
-        <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
+      <div className="relative px-4 py-3 lg:px-8 lg:py-5">
+        <div className="absolute flex flex-row space-x-1 -translate-y-1/2 lg:space-x-2 top-1/2">
+          <div className="w-2 h-2 bg-red-400 rounded-full lg:h-3 lg:w-3"></div>
+          <div className="w-2 h-2 bg-orange-400 rounded-full lg:h-3 lg:w-3"></div>
+          <div className="w-2 h-2 bg-green-200 rounded-full lg:h-3 lg:w-3"></div>
         </div>
         <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
           {project.name}
@@ -29,14 +29,14 @@ function ProjectCard({ project }) {
             <span className="text-gray-400">{'{'}</span>
           </div>
           <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+            <span className="ml-4 mr-2 text-white lg:ml-8">name:</span>
             <span className="text-gray-400">{`'`}</span>
             <span className="text-amber-300">{project.name}</span>
             <span className="text-gray-400">{`',`}</span>
           </div>
 
-          <div className="ml-4 lg:ml-8 mr-2">
-            <span className=" text-white">tools:</span>
+          <div className="ml-4 mr-2 lg:ml-8">
+            <span className="text-white ">tools:</span>
             <span className="text-gray-400">{` ['`}</span>
             {
               project.tools.map((tag, i) => (
@@ -52,11 +52,11 @@ function ProjectCard({ project }) {
             <span className="text-gray-400">{"],"}</span>
           </div>
           <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
+            <span className="ml-4 mr-2 text-white lg:ml-8">myRole:</span>
             <span className="text-orange-400">{project.role}</span>
             <span className="text-gray-400">,</span>
           </div>
-          <div className="ml-4 lg:ml-8 mr-2">
+          <div className="ml-4 mr-2 lg:ml-8">
             <span className="text-white">Description:</span>
             <span className="text-cyan-400">{' ' + project.description}</span>
             <span className="text-gray-400">,</span>
